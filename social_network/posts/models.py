4d9edd69e2a29061c56676b36de75ea7e7739349
Georgii Katunin <georgiymk82@gmail.com>
2025-08-06 19:24:42 +0300

    first commit

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,22 +1,37 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
+# реализуем 3 модели/таблицы: посты с возможностью фото, лайки и коменты
 
 class Post(models.Model):
-    ...
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    text = models.TextField()
+    image = models.ImageField(upload_to='posts/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-# для доп. задания
-# class PostImage(models.Model):
-#     ...
+    def __str__(self):
+        return f'{self.author.username} - {self.text[:30]}'
 
 
 class Like(models.Model):
-    ...
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.id}'
 
 
 class Comment(models.Model):
-    ...
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} on {self.post.id}'
