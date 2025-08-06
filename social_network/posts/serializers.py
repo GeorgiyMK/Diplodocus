ed563eb6a34dbf6116811c7c62acae64633235a2
Georgii Katunin <georgiymk82@gmail.com>
2025-08-06 20:37:57 +0300

    second commit

--- social_network/posts/serializers.py
+++ social_network/posts/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
+from .models import Post, Comment, Like
 
+# реализуем сериализаторы для API (переводчики в/c JSON)
 
 class CommentSerializer(serializers.ModelSerializer):
-    ...
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'text', 'created_at']
+        read_only_fields = ['user', 'created_at']
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
+        read_only_fields = ['user', 'created_at']
 
 
 class PostSerializer(serializers.ModelSerializer):
-    ...
+    author = serializers.StringRelatedField(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'text', 'image', 'created_at', 'likes_count', 'comments']
+        read_only_fields = ['author', 'created_at', 'likes_count', 'comments']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if not created:
+            like.delete()
+            return Response({'status': 'like removed'})
+        return Response({'status': 'liked'})
+
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class LikeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
